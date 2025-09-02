604f8d13dce34e5471521c23a71accda56b1abb1
Royal Joseph <royalprincecube@gmail.com>
2025-09-02 12:31:12 +0100

    navbar naming fixed in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 // frontend/src/App.jsx
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/NavBar';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
